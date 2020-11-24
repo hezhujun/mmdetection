996c2c16c8fb8730943ec68c98cf32c5878944c7
hezhujun <1214507717@qq.com>
2020-11-24 10:46:28 +0800

    tct, add bn in roi heads

--- mmdet/models/roi_heads/bbox_heads/__init__.py
+++ mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -7,6 +7,7 @@ from .relation_network_bbox_head import RelationNetworkBBoxHead
 from .relation_network_bbox_head_encoder import RelationNetworkBBoxHeadEncoder
 from .relation_network_bbox_head_without_geometry import RelationNetworkBBoxHeadWithoutGeometry
 from .roi_attention_bbox_head import *
+from .convfc_bn_bbox_head import *
 
 __all__ = [
     'BBoxHead', 'ConvFCBBoxHead', 'Shared2FCBBoxHead',
