fc45d69f74de1ae5db9670ff5cf485207abf5ff9
hezhujun <1214507717@qq.com>
2020-11-26 09:49:35 +0800

    tct head dropout

--- mmdet/models/roi_heads/bbox_heads/__init__.py
+++ mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -8,6 +8,7 @@ from .relation_network_bbox_head_encoder import RelationNetworkBBoxHeadEncoder
 from .relation_network_bbox_head_without_geometry import RelationNetworkBBoxHeadWithoutGeometry
 from .roi_attention_bbox_head import *
 from .convfc_bn_bbox_head import *
+from .convfc_dropout_bbox_head import *
 
 __all__ = [
     'BBoxHead', 'ConvFCBBoxHead', 'Shared2FCBBoxHead',
