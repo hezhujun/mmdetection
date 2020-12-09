c443d1e0be2f7a14fed280deef94ae5bb7319350
hezhujun <1214507717@qq.com>
2020-12-09 15:03:23 +0800

    relation network

---
+++ configs/coco/faster_rcnn_r50_fpn_relation_network_1x_coco.py
@@ -0,0 +1,37 @@
+_base_ = "../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py"
+model = dict(
+    roi_head=dict(
+        _delete_=True,
+        type='RelationNetworkRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='RelationNetworkBBoxHead',
+            in_channels=256,
+            roi_feat_size=7,
+            num_classes=80,
+            fc1_dim=1024,
+            fc2_dim=1024,
+            r1=1,
+            r2=0,
+            dk=64,
+            dg=64,
+            Nr=16,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0))))
+
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=2,)
+
+optimizer = dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0001)
+work_dir = "work_dir/coco/faster_rcnn_r50_fpn_relation_network_1x_coco/"
---
+++ configs/coco/faster_rcnn_r50_fpn_roi_attention_pixel_to_object_1x_coco.py
@@ -0,0 +1,16 @@
+_base_ = "../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py"
+model = dict(
+    roi_head=dict(
+        type='BatchRoIHead',
+        bbox_head=dict(
+            type='RoIAttentionPixelToObjectShared2FCBBoxHead',
+            attention_hidden_channels=256,
+        )
+    )
+)
+
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=2,)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+work_dir = "work_dir/coco/faster_rcnn_r50_fpn_roi_attention_pixel_to_object_1x_coco/"
